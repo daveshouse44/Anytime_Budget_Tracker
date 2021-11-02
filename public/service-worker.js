b5ea2a489a98da8bb509dc963f25c369a437a594
David Tierney <daveshouse44@hotmail.com>
2021-11-01 20:09:53 -0500

    added link to service worker and manifest in html, files to CACHE

---
+++ public/service-worker.js
@@ -0,0 +1,15 @@
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/index.js",
+  "/styles.css",
+  "/icons/icon-144x144.png",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+  "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+  "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
+  "/manifest.webmanifest",
+];
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
