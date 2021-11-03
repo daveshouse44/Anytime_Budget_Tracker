4b8310e53d1dc83e742f1355161583b415999a0a
David Tierney <daveshouse44@hotmail.com>
2021-11-02 23:22:40 -0500

    adjusted files to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,7 +4,7 @@ const FILES_TO_CACHE = [
   "/index.html",
   "/index.js",
   "/styles.css",
-  "/icons/icon-144x144.png",
+  "/db.js",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
   "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
